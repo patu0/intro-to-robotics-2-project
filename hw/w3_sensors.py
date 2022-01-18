1641396ae90403fabc18b4952bf46b597313cb84
Joshua R Bhagat Smith <bhagatsj@oregonstate.edu>
2022-01-18 09:55:11 -0800

    Updates and re-testing

--- hw/w3_sensors.py
+++ hw/w3_sensors.py
@@ -7,24 +7,39 @@ from picarx import Picarx
 from adc import ADC
 
 class Grayscale_Interpreter():
-    '''Container class for the grayscale interpreter values'''
-    def __init__(self, sensitivity, polarity):
-        self.sensitivity = sensitivity
-        self.polarity = polarity
+    """
+    Class to interpret the values from the greyscale cameras
+    ...
 
-    def get_line_status(self, fl_list):
-        if fl_list[0] > self.ref and fl_list[1] > self.ref and fl_list[2] > self.ref:
-            return 'stop'
-        elif fl_list[1] <= self.ref:
-            return 'forward'
-        elif fl_list[0] <= self.ref:
-            return 'right'
-        elif fl_list[2] <= self.ref:
+    Attributes
+    ----------
+    sensitivity : int
+        the expected difference between the line and the rest of the floor
+    polarity : [1,-1]
+        Direction of the sensitivity. Positive will look for a darker 
+        line and negative will look for a lighter line
+    """
+
+    def __init__(self, sensitivity, polarity=1):
+        self.sens = sensitivity
+        self.pol = polarity
+
+        assert((self.pol == 1) or (self.pol == -1))
+
+    def get_line_status(self, adc_list):          
+        diff1 = (adc_list[0]-adc_list[1])*self.pol
+        diff2 = (adc_list[2]-adc_list[1])*self.pol
+
+        if diff1 > self.sens:
             return 'left'
+        elif diff2 > self.sens:
+            return 'right'
+        else:
+            return 'unclear'
 
 class Controller():
     '''Class that controls the Picarx'''
-    def __init__(self, sensitivity, polarity, scale=1.0):
+    def __init__(self, car, sensitivity, polarity, scale=1.0):
         #Car object
         self.car = car
 
@@ -47,7 +62,9 @@ class Controller():
         self.car.set_dir_servo_angle(0)
 
     def test(self):
-        print(self.sensor.get_line_status())
+        adc_list = self.car.get_adc_value()
+        print(adc_list)
+        print(self.sensor.get_line_status(adc_list))
 
 def swerve_loop():
     print('drive back and forth over the line')
@@ -56,9 +73,8 @@ def drive_straight():
     print('drive straight')
 
 if __name__ == "__main__":
-    import time
     car = Picarx()
-    controller = Controller(car, 50, 220)
+    controller = Controller(car, 75, 1.0)
     while True:
         controller.test()
         time.sleep(1)
